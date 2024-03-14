74d16b702b9549ada332528328bee93a05a90a7d
focaG <60098824+Edamatec@users.noreply.github.com>
2024-03-14 11:23:27 -0300

    Feat: Criação do projeto

---
+++ src/main/java/posjavajdbcjpahibernate/App.java
@@ -0,0 +1,13 @@
+package posjavajdbcjpahibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
